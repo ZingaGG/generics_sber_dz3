f1e563bc471d9ffb0ae2cb91e7c2790e5db3a7e1
ZingaG <trad_bar@mail.ru>
2024-11-26 19:45:31 +0300

    CollectionUtil class realized. Main realized. Task 1 and Task 2are completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,110 @@
+
 package org.example;
+
+import java.util.*;
+
+import org.example.CollectionUtil;
+import org.example.Model.CountMapImpl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1:");
+
+
+        CountMapImpl<String> countMap = new CountMapImpl<>();
+
+        countMap.add("a");
+        countMap.add("b");
+        countMap.add("a");
+        countMap.add("c");
+        countMap.add("b");
+        countMap.add("a");
+
+        System.out.println("Count for 'a': " + countMap.getCount("a"));
+        System.out.println("Count for 'b': " + countMap.getCount("b"));
+        System.out.println("Count for 'c': " + countMap.getCount("c"));
+
+        System.out.println("Removing 'b', count was: " + countMap.remove("b"));
+        System.out.println("Count for 'b' after remove: " + countMap.getCount("b"));
+
+        System.out.println("Unique elements count: " + countMap.size()); // Ожидаемый вывод: 2 (apple, orange)
+
+        CountMapImpl<String> anotherCountMap = new CountMapImpl<>();
+        anotherCountMap.add("a");
+        anotherCountMap.add("b");
+        anotherCountMap.add("c");
+
+        countMap.addAll(anotherCountMap);
+
+        System.out.println("Count for 'a' after addAll: " + countMap.getCount("a"));  // Ожидаемый вывод: 4
+        System.out.println("Count for 'b' after addAll: " + countMap.getCount("b")); // Ожидаемый вывод: 1
+        System.out.println("Count for 'c' after addAll: " + countMap.getCount("c"));   // Ожидаемый вывод: 1
+
+        Map<String, Integer> resultMap = countMap.toMap();
+        System.out.println("Map from toMap(): " + resultMap);
+
+        Map<String, Integer> destinationMap = new HashMap<>();
+        countMap.toMap(destinationMap);
+        System.out.println("Destination Map after toMap(destination): " + destinationMap);
+
+
+        System.out.println("\n-------------------------------------");
+        System.out.println("\nTask 2:");
+
+        List<Integer> integers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21));
+        List<Integer> c2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        {
+            List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+            List<Integer> list2 = new ArrayList<>();
+            list1.add(6);
+            CollectionUtil.addAll(list1, list2);
+            System.out.println("Method 'addAll': " + list2);
+        }
+
+
+        System.out.println("Method 'indexOf': " + CollectionUtil.indexOf(integers, 1));
+
+        integers = CollectionUtil.limit(integers, 10);
+        System.out.println("Method 'limit': " + integers);
+
+        CollectionUtil.add(integers, 11);
+        System.out.println("Method 'add': " + integers);
+
+        CollectionUtil.removeAll(integers, c2);
+        System.out.println("Method 'removeAll': " + integers);
+
+        {
+            List<Integer> list = Arrays.asList(8, 1, 3, 5, 6, 4, 9, 10);
+            List<Integer> list2 = Arrays.asList(8, 1, 3, 5, 6, 4);
+            List<Integer> list3 = Arrays.asList(8, 1, 3, 2, 6, 4);
+            System.out.println("Method 'containsAll' with true result: " + CollectionUtil.containsAll(list, list2));
+            System.out.println("Method 'containsAll' with false result: " + CollectionUtil.containsAll(list, list3));
+        }
+
+        {
+            List<Integer> list = Arrays.asList(8, 1, 3, 5, 6, 4, 9, 10);
+            List<Integer> list2 = List.of(8);
+            List<Integer> list3 = List.of(2);
+            System.out.println("Method 'containsAny' with true result: " + CollectionUtil.containsAny(list, list2));
+            System.out.println("Method 'containsAny' with false result: " + CollectionUtil.containsAny(list, list3));
+        }
+
+        {
+            List<Integer> list = Arrays.asList(8, 1, 3, 5, 6, 4);
+            List<Integer> result = CollectionUtil.range(list, 3, 6);
+            System.out.println("Method 'range': "+ result);
+        }
+
+        {
+            List<Integer> list = Arrays.asList(8, 1, 3, 5, 6, 4);
+
+
+            Comparator<Integer> comparator = Integer::compare;
+
+
+            List<Integer> result = CollectionUtil.range(list, 3, 6, comparator);
+            System.out.println("Method 'range + comparator': "+ result);
+        }
     }
 }
--- src/main/java/org/example/Model/CollectionUtil.java
+++ src/main/java/org/example/Model/CollectionUtil.java
@@ -1,36 +1,83 @@
-package org.example.Model;
+package org.example;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectionUtil {
-    public static<T> void addAll(List<? extends T> source, List<? super T> destination) {
+    public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
         destination.addAll(source);
     }
 
-    public static List newArrayList() { }
+    public static List<?> newArrayList() {
+        return new ArrayList<>();
+    }
 
-    public static int indexOf(List source, Object o) { }
+    public static<T> int indexOf(List<? extends T> source, T t) {
 
-    public static List limit(List source, int size) { }
+        for (int i = 0; i < source.size(); i++) {
 
-    public static void add(List source, Object o) { }
+            if(!source.contains(t)){
+                return -1;
+            }
+            if(source.get(i).equals(t)){
+                return i;
+            }
+        }
+        return 0;
+    }
 
-    public static void removeAll(List removeFrom, List c2) { }
+    public static<T> List<T> limit(List<? extends T> source, int size) {
+        return source.stream().limit(size).collect(Collectors.toList());
+    }
+
+    public static <T> void add(List<? super T> source, T t) {
+        source.add(t);
+    }
+
+    public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> c2) {
+        removeFrom.removeAll(c2);
+    }
 
     //true если первый лист содержит все элементы второго
-    public static boolean containsAll(List c1, List c2) { }
+    public static <T> boolean containsAll(List<? super T> c1, List<? extends T> c2) {
+        return new HashSet<>(c1).containsAll(c2);
+    }
 
     //true если первый лист содержит хотя-бы 1 второго
-    public static boolean containsAny(List c1, List c2) { }
+    public static <T> boolean containsAny(List<? super T> c1, List<? extends T> c2) {
+        for(T elem : c2){
+            if(c1.contains(elem)){
+                return true;
+            }
+        }
 
+        return false;
+    }
+    //Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
+    // Элементы сравнивать через Comparable.
+    // Пример range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
+    public static <T extends Comparable<T>> List<T> range(List<? extends T> list, T min, T max) {
+        List<T> result = new ArrayList<>();
+        for (T elem : list) {
+            if (elem.compareTo(min) >= 0 && elem.compareTo(max) <= 0) {
+                result.add(elem);
+            }
+        }
+        return result;
+    }
     //Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
     // Элементы сравнивать через Comparable.
     // Прмер range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
-    public static List range(List list, Object min, Object max) { }
-
-    //Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
-    // Элементы сравнивать через Comparable.
-    // Прмер range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
-    public static List range(List list, Object min, Object max, Comparator comparator) { }
+    public static <T> List<T> range(List<? extends T> list, T min, T max, Comparator<? super T> comparator) {
+        List<T> result = new ArrayList<>();
+        for (T elem : list) {
+            if (comparator.compare(elem, min) >= 0 && comparator.compare(elem, max) <= 0) {
+                result.add(elem);
+            }
+        }
+        return result;
+    }
 }
